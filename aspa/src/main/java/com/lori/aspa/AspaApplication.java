51fb3975ac242476cc4bfead32a3a69b0a7ed572
lorela.shehu <lorela.shehu@DTI-PROGRAM-25.ASP.gov.al>
2018-04-25 11:51:00 +0200

    First commit

---
+++ aspa/src/main/java/com/lori/aspa/AspaApplication.java
@@ -0,0 +1,12 @@
+package com.lori.aspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspaApplication.class, args);
+	}
+}
